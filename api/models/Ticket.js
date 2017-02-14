e64e4a18221764110d4bca825dbcf974b37afd38
Rémi GATTAZ <remi.gattaz@gmail.com>
2017-02-14 17:01:56 +0100

    Add routes /tickets

--- api/models/Ticket.js
+++ api/models/Ticket.js
@@ -1,29 +1,27 @@
 /**
- * Package.js
+ * Ticket.js
  *
  * @description :: TODO: You might write a short summary of how this model works and what it represents here.
  * @docs        :: http://sailsjs.org/documentation/concepts/models-and-orm/models
  */
 
 module.exports = {
-  tableName: 'Packages',
+  tableName: 'Tickets',
   attributes: {
     user: {
       columnName: 'user_id',
       model: 'User',
       required: true
     },
-    ticket: {
-      columnName: 'ticket_id',
-      model: 'Ticket',
-      required: true,
-      unique: true
-    },
     infos: {
       columnName: 'info_id',
       model: 'Infos',
       required: true,
       unique: true
+    },
+    status: {
+      type: 'integer',
+      defaultsTo: 0
     }
   }
 };
