96b039a1a5c46a03d85a7d620b1888c2bca9084a
Rémi GATTAZ <remi.gattaz@gmail.com>
2017-02-17 17:01:29 +0100

    Use promises to check Messages validity

--- api/models/Message.js
+++ api/models/Message.js
@@ -1,3 +1,4 @@
+var Promise = require('bluebird')
 /**
  * Message.js
  *
@@ -25,15 +26,57 @@ module.exports = {
   },
   /* Check complex conditions before persisting the Object in the database */
   beforeValidate : function(values, next) {
-    /* Check the either package or ticket is set */
-    if( values.ticket == undefined && values.package == undefined) {
-      sails.log.info("Creation of Message failed because package or ticket should either be set");
-      next("ERROR : Creation of Message failed because package or ticket should either be set");
-    } else if (values.ticket != undefined && values.package != undefined) {
-      sails.log.info("Creation of Message failed because package and ticket can't be both set");
-      next("ERROR : Creation of Message failed because package and ticket can't be both set");
-    }else{
-      next();
-    }
+    var promises = [];
+
+    /* Check that either package or ticket is set */
+    promises.push(new Promise(function (resolve, reject) {
+      if( values.ticket == undefined && values.package == undefined) {
+        return reject("ERROR : Creation of Message failed because package or ticket should either be set");
+      } else if (values.ticket != undefined && values.package != undefined) {
+        return reject("ERROR : Creation of Message failed because package and ticket can't be both set");
+      }
+      resolve();
+    }));
+
+    /* If value package is set, it should point to an existing package */
+    promises.push(new Promise(function (resolve, reject) {
+      if(values.package != undefined) {
+        Package
+          .findOne({id: values.package})
+          .then(function (record) {
+            if(record == undefined) {
+              return reject("ERROR : Creation of Message failed because package doesn't exist");
+            }
+            resolve();
+          })
+          .catch(function (err) { reject(err) });
+      }
+    }));
+
+    /* If value ticket is set, it should point to an existing ticket */
+    promises.push(new Promise(function (resolve, reject) {
+      if(values.ticket != undefined) {
+        Ticket
+          .findOne({id: values.ticket})
+          .then(function (record) {
+            if(record == undefined) {
+              return reject("ERROR : Creation of Message failed because ticket doesn't exist");
+            }
+            resolve();
+          })
+          .catch(function (err) { reject(err) });
+      }
+    }));
+
+    /* Wait for all promises call next if no error */
+    Promise.all(promises)
+      .spread(function(){
+        next();
+      })
+      .catch(function(err){
+        /* At least one promise threw an error */
+        sails.log.info(err);
+        next(err)
+      });
   }
 };
